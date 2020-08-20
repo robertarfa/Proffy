aedaaa6f3bcae4125082846808aef00abe6441ec
Roberta Aguilar <roberta_rfa@hotmail.com>
2020-08-20 20:02:28 -0300

    Landing Page

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+- Utilizando tecnologia mobile first.
 
 ## Available Scripts
 
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+import PageHeader from '../../components/PageHeader'
+
+export default function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis." />
+    </div>
+  )
+}
