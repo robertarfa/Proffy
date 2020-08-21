7c394f25e824057b54ccc3d4d5f4138e2d862922
Roberta Aguilar <roberta_rfa@hotmail.com>
2020-08-21 20:21:21 -0300

    frontend web

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,24 +3,16 @@ import './styles.css'
 
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem'
+import Input from '../../components/Input'
 
 export default function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da Semana</label>
-            <input type="text" id="week_day" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Input name="subject" label="Matérias" />
+          <Input name="week_day" label="Dia da Semana" />
+          <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
 
